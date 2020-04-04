d533dd1867466bd27551402aa2a8717eaeec70be
tsharon-byte <sharonova_t@inbox.ru>
2020-04-04 21:00:51 +0300

    Updates

---
+++ precache-manifest.edbadfd947d93837eea6393878dc7475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b579e7eaed07d17c6e3e267eb8c458f",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "83f5b4ef8db31192acf0",
+    "url": "/drum-machine/static/css/main.debd8694.chunk.css"
+  },
+  {
+    "revision": "7e8e4ec555f7678b100f",
+    "url": "/drum-machine/static/js/2.86e07a12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.86e07a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f5b4ef8db31192acf0",
+    "url": "/drum-machine/static/js/main.a0ee8e66.chunk.js"
+  },
+  {
+    "revision": "315e3dcde1df1090667c",
+    "url": "/drum-machine/static/js/runtime-main.1958076e.js"
+  }
+]);
